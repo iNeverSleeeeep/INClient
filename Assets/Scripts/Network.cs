396950828c2ad6fe0d6c31b0bd6c3ab81670afbb
INeverSleeeep <362761534@qq.com>
2020-01-22 13:26:23 +0800

    网络相关代码

---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+
+public class Network
+{
+    private Queue<byte[]> SendBuffer = new Queue<byte[]>();
+    private Queue<byte[]> ReveBuffer = new Queue<byte[]>();
+
+    public void Send(byte[] bytes)
+    {
+        lock (SendBuffer)
+        {
+            SendBuffer.Enqueue(bytes);
+        }
+    }
+
+    public byte[] Recv()
+    {
+        lock (ReveBuffer)
+        {
+            if (ReveBuffer.Count > 0)
+                return ReveBuffer.Dequeue();
+            return null;
+        }
+    }
+
+
+    public Network(string ip, int port)
+    {
+        Init(new IPEndPoint(IPAddress.Parse(ip), port));
+    }
+
+    public Network(IPEndPoint point)
+    {
+        Init(point);
+    }
+
+    private void Init(IPEndPoint p)
+    {
+        this.point = p;
+        sendThread = new Thread(SendThread);
+        recvThread = new Thread(ReceiveThread);
+        sendThread.Start();
+        recvThread.Start();
+    }
+
+    public void Abort()
+    {
+        sendThread.Abort();
+        recvThread.Abort();
+        socket.Close();
+    }
+
+    /// <summary>
+    /// 套接字
+    /// </summary>
+    private Socket socket;
+    IPEndPoint point;
+
+    private Thread sendThread;
+    private Thread recvThread;
+    /// <summary>
+    /// 连接服务器
+    /// </summary>
+    public void Connect()
+    {
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Connect(point);
+    }
+
+    public void ReceiveThread()
+    {
+        byte[] ser_msg = new byte[1024];
+        int current = 0;
+        while (true)
+        {
+            while (current < 2)
+            {
+                int count = socket.Receive(ser_msg, current, 1024 - current, SocketFlags.None);
+                current += count;
+            }
+
+            var size = BitConverter.ToUInt16(ser_msg, 0);
+            if (BitConverter.IsLittleEndian)
+            {
+                var buf = BitConverter.GetBytes(size);
+                Array.Reverse(buf);
+                size = BitConverter.ToUInt16(buf, 0);
+            }
+
+            while (current - 2 < size)
+            {
+                int count = socket.Receive(ser_msg, current, SocketFlags.None);
+                current += count;
+            }
+
+            var buffer = new byte[size];
+            Array.Copy(ser_msg, 2, buffer, 0, size);
+            lock (ReveBuffer)
+                ReveBuffer.Enqueue(buffer);
+
+            Array.Copy(ser_msg, size + 2, ser_msg, 0, current - size - 2);
+            current = current - size - 2;
+        }
+    }
+
+    public void SendThread()
+    {
+        while (true)
+        {
+            byte[] bytes = null;
+            lock (SendBuffer)
+            {
+                if (SendBuffer.Count > 0)
+                    bytes = SendBuffer.Dequeue();
+            }
+            if (bytes != null)
+            {
+                var size = BitConverter.GetBytes((short)(bytes.Length));
+                if (BitConverter.IsLittleEndian)
+                    Array.Reverse(size);
+                var buffer = new byte[bytes.Length + 2];
+                Array.Copy(size, buffer, 2);
+                Array.Copy(bytes, 0, buffer, 2, bytes.Length);
+                socket.Send(buffer);
+            }
+            else
+            {
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NetworkMgr.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkMgr : MonoBehaviour
+{
+    public Network Login;
+    public Network Game;
+
+    private static NetworkMgr instance;
+    public static NetworkMgr Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                var go = new GameObject("NetworkMgr");
+                DontDestroyOnLoad(go);
+                instance = go.AddComponent<NetworkMgr>();
+            }
+            return instance;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CloseLogin();
+        CloseGame();
+    }
+
+    public void ConnectLogin(string ip, int port)
+    {
+        Login = new Network(ip, port);
+    }
+
+    public void CloseLogin()
+    {
+        if (Login != null)
+        {
+            Login.Abort();
+            Login = null;
+        }
+    }
+
+    public void ConnectGame(string ip, int port)
+    {
+        Game = new Network(ip, port);
+    }
+
+    public void CloseGame()
+    {
+        if (Game != null)
+        {
+            Game.Abort();
+            Game = null;
+        }
+    }
+}
